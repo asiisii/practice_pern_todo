de8af306eb27a63a821fcdccf43db5f0b75c8592
Ashish Malla <ashishmalla45@gmail.com>
2023-01-12 23:34:50 -0700

    route to add a new todo

---
+++ server/routes/createTodoRoute.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express'
+import client from '../config/db'
+
+const createTodo = async (req: Request, res: Response) => {
+	try {
+		const { text } = req.body
+		const newTodo = await client.query('INSERT INTO todo (text) VALUES($1)', [
+			text,
+		])
+    console.log('====================================');
+    console.log({newTodo});
+    console.log('====================================');
+		res.status(201).json(newTodo.rows[0])
+	} catch (error) {
+		console.log('====================================')
+		console.log({ caughtErr: error })
+		console.log('====================================')
+		res.status(400).json({ message: error })
+		console.log('====================================')
+	}
+}
+
+export default createTodo
