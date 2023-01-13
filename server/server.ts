0451189a494be2a334946df98ed92979fe06ebb9
Ashish Malla <ashishmalla45@gmail.com>
2023-01-12 23:35:32 -0700

    use routes to test get and post request in postman

--- server/server.ts
+++ server/server.ts
@@ -1,37 +1,43 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
 import cors from 'cors'
-import client from './db.js'
+import router from './routes'
+import client from './config/db'
+
+client.connect(err => {
+	if (err) {
+		console.error('connection error', err.stack)
+	} else {
+		client.query('SELECT current_database()', (err, res) => {
+			if (err) {
+				console.error('error running query', err)
+			} else {
+				console.log('Connected to ' + res.rows[0].current_database)
+			}
+			// client.end()
+		})
+	}
+})
+
+dotenv.config()
 
 // Create an express app
 const app = express()
-
-const port = 3000
 
 //middleware
 app.use(cors())
 app.use(express.json())
 
 //routes
-
-app.post('/todos', async (req, res) => {
-  
+app.get('/', (req: Request, res: Response) => {
+	res.status(200).send('API is running...')
 })
+app.use(router)
 
-// create new todo
-app.post('/todos', async (req, res) => {
-	try {
-	} catch (error) {
-		console.log('====================================')
-		// console.error(error.message)
-    console.log(error);
-    console.log('====================================');
-	}
-})
+const PORT = process.env.PORT || 5000
 
-// get todos
-
-app.listen(5000, () => {
+app.listen(PORT, () => {
 	console.log('====================================')
-	console.log('server has started on port 5000')
+	console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
 	console.log('====================================')
 })
