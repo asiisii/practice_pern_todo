4ea25ea6697088a29c983ab086e8e03a1efcb532
Ashish Malla <ashishmalla45@gmail.com>
2023-01-13 20:10:12 -0700

    add deleteTodo to the route list

--- server/routes.ts
+++ server/routes.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import getTodos from './routes/getTodosRoute'
 import createTodo from './routes/createTodoRoute'
+import deleteTodo from './routes/deleteTodoRoute'
 
 const router = express.Router()
 
 router.get('/todos', getTodos)
 router.post('/todos', createTodo)
+router.delete('/todos/:id', deleteTodo)
 
 export default router
