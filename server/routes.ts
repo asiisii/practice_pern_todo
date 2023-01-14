f9b838956ccfcc857e60732396a2cdccadf599d7
Ashish Malla <ashishmalla45@gmail.com>
2023-01-14 12:42:40 -0700

    use updatexttodo route to update the text

--- server/routes.ts
+++ server/routes.ts
@@ -2,11 +2,13 @@ import express from 'express'
 import getTodos from './routes/getTodosRoute'
 import createTodo from './routes/createTodoRoute'
 import deleteTodo from './routes/deleteTodoRoute'
+import { updateTextTodo } from './routes/updateTodoRoute'
 
 const router = express.Router()
 
 router.get('/todos', getTodos)
 router.post('/todos', createTodo)
 router.delete('/todos/:id', deleteTodo)
+router.patch('/todos/:id', updateTextTodo)
 
 export default router
