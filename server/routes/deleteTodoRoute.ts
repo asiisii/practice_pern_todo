e736bdd266bc50b692e07337021428e9d255fd66
Ashish Malla <ashishmalla45@gmail.com>
2023-01-14 12:40:23 -0700

    add route to delete a todo by id

--- server/routes/deleteTodoRoute.ts
+++ server/routes/deleteTodoRoute.ts
@@ -4,6 +4,7 @@ import client from '../config/db'
 const deleteTodo = async (req: Request, res: Response) => {
 	try {
 		const { id } = req.params
+    console.error(req.params)
 		await client.query('DELETE FROM todo WHERE todoid = $1', [id])
 		res.json({ message: 'Todo Deleted' })
 	} catch (error) {
