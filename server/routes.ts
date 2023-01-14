fd31b7dd3e4f2d7a88b8250b61fd9071fafa925a
Ashish Malla <ashishmalla45@gmail.com>
2023-01-14 12:43:35 -0700

    add route to toggle iscomplete field

--- server/routes.ts
+++ server/routes.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import getTodos from './routes/getTodosRoute'
 import createTodo from './routes/createTodoRoute'
 import deleteTodo from './routes/deleteTodoRoute'
-import { updateTextTodo } from './routes/updateTodoRoute'
+import { updateTextTodo, updateIsCompleteTodo } from './routes/updateTodoRoute'
 
 const router = express.Router()
 
@@ -10,5 +10,6 @@ router.get('/todos', getTodos)
 router.post('/todos', createTodo)
 router.delete('/todos/:id', deleteTodo)
 router.patch('/todos/:id', updateTextTodo)
+router.patch('/todos/:id/complete', updateIsCompleteTodo)
 
 export default router
